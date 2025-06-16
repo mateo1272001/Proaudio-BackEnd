080ddfe4c774995489d89b1aeadfd3c6a2ffde30
mateo1272001 <mateovazquezreyes@gmail.com>
2025-06-16 01:01:30 -0300

    environment test

--- proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
+++ proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
@@ -1,23 +1,23 @@
-package com.ProyectoIntegradorBE.proaudioBE.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://test.d287o0p9ndx0gm.amplifyapp.com")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//package com.ProyectoIntegradorBE.proaudioBE.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("https://test.d287o0p9ndx0gm.amplifyapp.com")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
