b1f5d5f6b01f091df1bb22f3387b58f0a1920d69
mateo1272001 <mateovazquezreyes@gmail.com>
2025-10-31 18:34:37 -0300

    cors config

--- proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
+++ proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
@@ -1,21 +1,14 @@
 package com.ProyectoIntegradorBE.proaudioBE.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*").allowedOrigins("https://proaudiochannels.com.uy")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5001") // o "*" si es solo para tests
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
     }
 }
