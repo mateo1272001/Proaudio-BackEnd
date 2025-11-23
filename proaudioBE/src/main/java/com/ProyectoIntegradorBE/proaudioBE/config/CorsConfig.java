0e5b5ed9c5f35556997004b4bb12a105220aee19
mateo1272001 <mateovazquezreyes@gmail.com>
2025-11-23 13:53:18 -0300

    testing allowed origins

--- proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
+++ proaudioBE/src/main/java/com/ProyectoIntegradorBE/proaudioBE/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5001") // o "*" si es solo para tests
+        registry.addMapping("/**").allowedOrigins("*") // o "*" si es solo para tests
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
     }
 }
